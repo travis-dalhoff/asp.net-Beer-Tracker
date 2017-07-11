6bdcf263f979799a14752a358a04148a257623d5
Travis Dalhoff <tdalhoff@live.com>
2017-07-11 16:29:06 -0400

    Updated Beers Detail view

--- BeerTracker/Controllers/BeersController.cs
+++ BeerTracker/Controllers/BeersController.cs
@@ -11,6 +11,13 @@ namespace BeerTracker.Controllers
         public ActionResult Detail()
         {
 
+            ViewBag.BeerName = "Breakfast Stout";
+            ViewBag.BreweryName = "Founders";
+            ViewBag.Style = "Imperial Stout";
+            ViewBag.Ibu = 60;
+            ViewBag.Abv = 8.3;
+            ViewBag.Description = "<p>Really good beer!</p>";
+
             return View();
 
         }
